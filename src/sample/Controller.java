9d5ef6bdb7ba3a71a8d5d58b238c4c3f5ce68f5a
Magnus Tidemann <magnuti@stud.ntnu.no>
2019-03-12 12:00:01 +0100

    New project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.*;
+
 public class Controller {
+
+    @FXML
+    TextField textInput;
+
+    @FXML
+    TextField textOutput;
+
+    public void onButtonClick(){
+        textOutput.setText(textInput.getText());
+        textInput.setText("");
+    }
 }
